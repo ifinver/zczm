5dfbe462fb33209281dcc3a064618ef1399ca661
v <dragonstar0369@gmail.com>
2025-01-30 18:56:55 +0800

    341234234

--- frontend/src/static/js/pages/ManageCommentsPage.js
+++ frontend/src/static/js/pages/ManageCommentsPage.js
@@ -41,7 +41,7 @@ export class ManageCommentsPage extends Page {
       false,
       (response) => {
         console.log(response)
-        if (response && response.msg === 'ok') {
+        if (response && response.data && response.data.msg === 'ok') {
           PageActions.addNotification('导航配置修改成功.', '提示');
         } else {
           if(response && response.msg){
@@ -71,7 +71,7 @@ export class ManageCommentsPage extends Page {
       false,
       (response) => {
         console.log(response)
-        if (response && response.msg === 'ok') {
+        if (response && response.data && response.data.msg === 'ok') {
           PageActions.addNotification('弹框配置修改成功.', '提示');
         } else {
           if(response && response.msg){
