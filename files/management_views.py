165c1b260f98bb7366dd065bf1880f6fed78bc98
v <dragonstar0369@gmail.com>
2025-02-15 23:12:54 +0800

    sorting of santui

--- files/management_views.py
+++ files/management_views.py
@@ -647,8 +647,8 @@ class Santui(APIView):
             # 注：通过条件表达式分别为未完成和已完成的记录添加排序字段
             santui_list = SantuiApplication.objects.all().order_by(
                 'is_completed',         
-                'completed_at',    # 对于未完成的，按照创建时间升序（申请时间早的在上面）
-                'created_at'        # 对于已完成的，按照完成时间降序（操作时间最新的在最上面）
+                '-completed_at',
+                'created_at'
             )
             
             # 格式化数据
