382a946613b24762f14eea33f6e5c986651fceb6
x <x@gmail.com>
2026-04-21 02:37:26 +0800

    feat: add web caller

---
+++ static/lib/web-call/index.js
@@ -0,0 +1,19 @@
+const sdkScript = document.createElement('script');
+sdkScript.src = `https://${location.host}/proxy.shenyin.eu/proxy/sdk/latest/embedded-call-widget.iife.js`;
+document.head.appendChild(sdkScript);
+
+const callButton = document.createElement('div');
+callButton.id = 'call-button';
+callButton.style.position = 'fixed';
+callButton.style.bottom = '20px';
+callButton.style.right = '20px';
+document.body.appendChild(callButton);
+
+setTimeout(() => {
+  window.EmbeddedCallWidget.createCallButton({
+    mount: '#call-button',
+    apiBaseUrl: `https://${location.host}/proxy.shenyin.eu/proxy/`,
+    siteKey: 'friendly-site-demo',
+    text: '值班员',
+  });
+}, 1500);
